333094da6f53a52025bf11b4d1bf9fc17c0f570b
DigaLugas <lucasgustavorodrigues10@gmail.com>
2024-05-20 00:23:14 -0300

    added investimentos iframe

--- script/dashboard.js
+++ script/dashboard.js
@@ -5,3 +5,10 @@ document.getElementById('pagamentosButton').addEventListener('click', function()
     var top = (screen.height - popupHeight) / 2;
     window.open('../iframes/pagamentos.html', 'Pagamento', 'width=' + popupWidth + ',height=' + popupHeight + ',top=' + top + ',left=' + left);
 });
+document.getElementById('investimentosButton').addEventListener('click', function() {
+    var popupWidth = 600;
+    var popupHeight = 700;
+    var left = (screen.width - popupWidth) / 2;
+    var top = (screen.height - popupHeight) / 2;
+    window.open('../iframes/investimentos.html', 'Pagamento', 'width=' + popupWidth + ',height=' + popupHeight + ',top=' + top + ',left=' + left);
+});
